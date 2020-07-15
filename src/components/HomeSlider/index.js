220efd3851e6ec9af8f4211110e4abfe3cf5d41d
Mathew Ramirez <Mathew.J.Ramirez@gmail.com>
2020-07-15 11:37:15 -0500

    fixed the margins of the body, and also messed with image positioning

--- src/components/HomeSlider/index.js
+++ src/components/HomeSlider/index.js
@@ -17,20 +17,28 @@ export function HomeSlider() {
         <div className="carousel-inner">
           <div className="carousel-item active">
             <img
-              className="d-block w-100"
-              src="https://via.placeholder.com/150"
+              className="d-block w-100 slideimage"
+              src="https://www.accessselfstorage.com/wp-content/uploads/2017/06/Bernardsville-Tempo-Truck-2.jpg"
               alt="First slide"
             />
-              <div className="carousel-caption d-none d-md-block bg-dark mb-4">
-                <h5>bdhsuabfsa</h5>
-                <p>fdshauifgdsgayig</p>
-              </div>
+            <div className="carousel-caption d-none d-md-block bg-dark mb-4">
+              <h5>bdhsuabfsa</h5>
+              <p>fdshauifgdsgayig</p>
+            </div>
           </div>
           <div className="carousel-item">
-            <img className="d-block w-100" src="" alt="Second slide" />
+            <img
+              className="d-block w-100 slideimage"
+              src="https://uploads.website.storedge.com/17c74b4e-229a-4eb3-aa2b-ec6187254c6c/image005-1.jpg"
+              alt="Second slide"
+            />
           </div>
           <div className="carousel-item">
-            <img classname="d-block w-100" src="" alt="Third slide" />
+            <img
+              classname="d-block w-100 slideimage"
+              src="https://www.accessselfstorage.com/wp-content/uploads/2017/06/Bernardsville-Tempo-Truck-2.jpg"
+              alt="Third slide"
+            />
           </div>
         </div>
         <a
