6dfdd86954f2fa5d44fb0409661d94df8cf9bbbd
Mathew Ramirez <Mathew.J.Ramirez@gmail.com>
2020-10-01 11:48:01 -0500

    removed unsecured logo

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,11 +7,6 @@ export function Contact() {
       <nav className="navbar">
         <div>
           <a href="#">
-            <img
-              src="http://beta.movinon.info.php72-24.phx1-2.websitetestlink.com/wp-content/uploads/2020/06/cropped-movinonnewlogogoldoragangetanbackround.jpg"
-              alt="Movin On Logo"
-              href=""
-            />
           </a>
           <h1>Movin On Movers</h1>
           <p>We Care About Your Cargo!</p>
